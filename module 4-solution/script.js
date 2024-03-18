88285295637535a500abfa0f837528810d5833b3
Nat <157559625+nerdycode2@users.noreply.github.com>
2024-03-18 19:18:30 -0400

    Add files via upload

---
+++ module 4-solution/script.js
@@ -0,0 +1,67 @@
+// Module 4 Assignment Instructions.
+// The idea of this assignment is to take an existing array of names
+// and then output either Hello 'Name' or Good Bye 'Name' to the console.
+// The program should say "Hello" to any name except names that start with a "J"
+// or "j", otherwise, the program should say "Good Bye". So, the final output
+// on the console should look like this:
+/*
+Hello Yaakov
+Good Bye John
+Good Bye Jen
+Good Bye Jason
+Hello Paul
+Hello Frank
+Hello Larry
+Hello Paula
+Hello Laura
+Good Bye Jim
+
+*/
+// STEP 1: (NOTHING TO DO. ALREADY DONE FOR YOU)
+// See Lecture 52, part 2
+// (Note, Step 2 will be done in the SpeakHello.js file.)
+(function () {
+
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+    
+    for (var i = 0; i <names.length; i++) {
+   
+      var firstLetter = names[i].charAt(0).toLowerCase();
+
+      if (firstLetter === 'j') {
+        console.log( "Goodbye"+ names[i]);
+      } else {
+        console.log(" Hello"+ names[i])
+      }
+    }
+    
+    })();
+
+
+   // STEP 11: (NOTHING TO DO. ALREADY DONE FOR YOU)
+      // Retrieve the first letter of the current name in the loop.
+      // Use the string object's 'charAt' function. Since we are looking for
+      // names that start with either upper case or lower case 'J'/'j', call
+      // string object's 'toLowerCase' method on the result so we can compare
+      // to lower case character 'j' afterwards.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
